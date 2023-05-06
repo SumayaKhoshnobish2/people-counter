7087da21a2bfa3daa2155347c4e8d1ccef842953
SumayaKhoshnobish2 <112729989+SumayaKhoshnobish2@users.noreply.github.com>
2023-05-05 23:45:46 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el") // pass in arguments
+
+console.log(saveEl)
+
+
+let count = 0
+
+function increment() {
+  count = count + 1
+  countEl.textContent = count
+}
+function save(){
+  let countStr = " " + count + " - "
+  
+  saveEl.textContent += countStr
+
+  countEl.textContent = 0
+  count = 0
+    
+  console.log(count)
+  
+}
